548d41b0da91f6c298ee1d51186ab84914be1dcd
toballz <42551340+toballz@users.noreply.github.com>
2019-12-18 19:45:47 -0500

    Add files via upload

---
+++ Edit Host/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Edit_Host
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.domain2add = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // domain2add
+            // 
+            this.domain2add.Location = new System.Drawing.Point(12, 12);
+            this.domain2add.Multiline = true;
+            this.domain2add.Name = "domain2add";
+            this.domain2add.Size = new System.Drawing.Size(198, 51);
+            this.domain2add.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Location = new System.Drawing.Point(66, 69);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 31);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(222, 110);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.domain2add);
+            this.MaximumSize = new System.Drawing.Size(238, 149);
+            this.MinimumSize = new System.Drawing.Size(238, 149);
+            this.Name = "Form1";
+            this.Text = "Edit Hosts";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox domain2add;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Edit Host/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Edit_Host
+{
+    public partial class Form1 : Form
+    {
+        private const string _kip = "211.222.233.244";
+        private const string _watermark = "#by_edithost";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string _domain2add = new Uri(domain2add.Text).Host.Trim();
+
+                string _hostfile = File.ReadAllText(@"C:\Windows\System32\drivers\etc\hosts");
+                if (_hostfile.Contains(_domain2add) == false)
+                {
+                    using (StreamWriter sw = File.AppendText(@"C:\Windows\System32\drivers\etc\hosts"))
+                    {
+                        sw.WriteLine(_kip + " " + _domain2add + " " + _watermark);
+                        domain2add.Text = _domain2add;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Domain already there.");
+                }
+            }catch(Exception ee)
+            {
+                MessageBox.Show(ee.Message);
+            }
+        }
+    }
+}
